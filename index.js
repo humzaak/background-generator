b3bdf6676ba23dd11eab6e698bbcfbb0e662d44e
Humza Akhtar <humzaakhtar1@gmail.com>
2018-08-26 18:44:01 +0800

    added files

---
+++ index.js
@@ -0,0 +1,18 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.querySelector("body");
+
+
+function SetGradient(){
+	body.style.background = "linear-gradient(to right, " + 
+	color1.value + "," + color2.value  + ")";
+
+
+}
+
+color1.addEventListener("input",SetGradient)
+
+
+color2.addEventListener("input",SetGradient)
+
